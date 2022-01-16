40bbe81974abf45a8c61e0e2ffc2885d90b2c122
Darfune <harolep@gmail.com>
2022-01-16 14:15:53 +0200

    First FingerPrint Authentication

--- app/src/main/java/com/example/passwordmanagerapp/MainActivity.java
+++ app/src/main/java/com/example/passwordmanagerapp/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.passwordmanagerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -14,6 +21,7 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.passwordmanagerapp.Adapters.AppItemAdapter;
 import com.example.passwordmanagerapp.Models.AppItem;
@@ -27,6 +35,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity implements AppItemAdapter.OnItemListener{
 
@@ -40,10 +49,21 @@ public class MainActivity extends AppCompatActivity implements AppItemAdapter.On
     private static final String filename = "SecureDataFile";
     private Boolean authorization = false;
 
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+    private ConstraintLayout mainActivityLayout;
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainActivityLayout = findViewById(R.id.mainActivity);
+
+
+
+        FingerPrintAuth();
 
         addItemFloatingActionButton = (FloatingActionButton) findViewById(R.id.addItemFloatingActionButton);
 
@@ -57,13 +77,13 @@ public class MainActivity extends AppCompatActivity implements AppItemAdapter.On
 
         itemRecyclerView.setAdapter(appItemAdapter);
 
-        if (authorization){
-            try {
-                showUsersData();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+
+        try {
+            showUsersData();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+
 
 
 
@@ -91,12 +111,69 @@ public class MainActivity extends AppCompatActivity implements AppItemAdapter.On
 
     }
 
+    private void FingerPrintAuth() {
+
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this,"Device doesn't have fingerprint",Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this,"Fingerprint hardware not working",Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(this,"No FingerPrint Assigned",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        mainActivityLayout.setVisibility(View.INVISIBLE);
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),"Authentication Error",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                mainActivityLayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(),"Authentication Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("FingerPrint Authentication")
+                .setDescription("Use FingerPrint to Login").setDeviceCredentialAllowed(true).build();
+
+        biometricPrompt.authenticate(promptInfo);
+
+    }
+
     @Override
     protected void onRestart() {
         super.onRestart();
+        FingerPrintAuth();
         try {
+            showUsersData();
 
-                showUsersData();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FingerPrintAuth();
+        try {
+            showUsersData();
 
         } catch (JSONException e) {
             e.printStackTrace();
