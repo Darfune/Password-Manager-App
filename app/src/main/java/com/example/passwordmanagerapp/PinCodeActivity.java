a7d1268461012093f326b2e84b1c22bd4b325902
Darfune <harolep@gmail.com>
2022-01-17 22:34:40 +0200

    Fix Validation Input Fields

--- app/src/main/java/com/example/passwordmanagerapp/PinCodeActivity.java
+++ app/src/main/java/com/example/passwordmanagerapp/PinCodeActivity.java
@@ -12,9 +12,13 @@ import androidx.core.content.ContextCompat;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.Settings;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +26,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.concurrent.Executor;
+
+import javax.security.auth.login.LoginException;
 
 public class PinCodeActivity extends AppCompatActivity {
 
@@ -33,15 +39,18 @@ public class PinCodeActivity extends AppCompatActivity {
     private EditText editTextNumberPassword2;
     private EditText editTextNumberPassword3;
     private EditText editTextNumberPassword4;
-    private Button enterButton;
-    private Boolean reEnter = false;
+
     private String digits;
-    private String reDigits;
+    private Boolean validationFlag = false;
+
     private SharePrefEncryption pinCode;
     private static final String filename = "SecureDataFile";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin_code);
 
@@ -50,20 +59,18 @@ public class PinCodeActivity extends AppCompatActivity {
         editTextNumberPassword2 = findViewById(R.id.editTextNumberPassword2);
         editTextNumberPassword3 = findViewById(R.id.editTextNumberPassword3);
         editTextNumberPassword4 = findViewById(R.id.editTextNumberPassword4);
-        enterButton = findViewById(R.id.enterButton);
 
         pinCode = new SharePrefEncryption(this,filename);
-        String pin = pinCode.fetchPin();
-        Toast.makeText(this,pin,Toast.LENGTH_SHORT).show();
+        String usersPinCode = pinCode.fetchPin();
 
-        if (pin.isEmpty()){
-            newPinCode();
-        }
-        else{
-            getPinCode(pin);
+        if (pinCode.isBiometricsAllowed()){
+            biometricAuth();
         }
 
+        setupPinInputs(usersPinCode);
     }
+
+
 
     private void fingerprintDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -72,6 +79,7 @@ public class PinCodeActivity extends AppCompatActivity {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         pinCode.biometricsVerification(true);
+                        Log.i("onClick: ", "yes");
                         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                         startActivity(intent);
                     }
@@ -80,6 +88,7 @@ public class PinCodeActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.cancel();
                         pinCode.biometricsVerification(false);
+                        Log.i("onClick: ", "no");
                         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                         startActivity(intent);
                     }
@@ -88,76 +97,138 @@ public class PinCodeActivity extends AppCompatActivity {
         alert.show();
     }
 
-    private void getPinCode(String pin) {
+    private String readUsersInput(){
+        String d;
+        return d = String.valueOf(editTextNumberPassword1.getText())
+                + String.valueOf(editTextNumberPassword2.getText())
+                + String.valueOf(editTextNumberPassword3.getText())
+                + String.valueOf(editTextNumberPassword4.getText());
 
-        if (pinCode.isBiometricsAllowed()){
-            biometricAuth();
-        }
-
-        enterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                digits = String.valueOf(editTextNumberPassword1.getText())
-                        + String.valueOf(editTextNumberPassword2.getText())
-                        + String.valueOf(editTextNumberPassword3.getText())
-                        + String.valueOf(editTextNumberPassword4.getText());
-                if (digits.equals(pin)){
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"the Pin Code is incorrect",Toast.LENGTH_SHORT).show();
-                    editTextNumberPassword1.setText("");
-                    editTextNumberPassword2.setText("");
-                    editTextNumberPassword3.setText("");
-                    editTextNumberPassword4.setText("");
-                }
-            }
-        });
     }
 
-    private void newPinCode() {
+    private void emptyTheFields(){
+        editTextNumberPassword1.setText("");
+        editTextNumberPassword2.setText("");
+        editTextNumberPassword3.setText("");
+        editTextNumberPassword4.setText("");
+    }
 
-        enterButton.setOnClickListener(new View.OnClickListener() {
+    private void setupPinInputs(String pin){
+
+        editTextNumberPassword1.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                if (!reEnter){
-                    digits = String.valueOf(editTextNumberPassword1.getText())
-                            + String.valueOf(editTextNumberPassword2.getText())
-                            + String.valueOf(editTextNumberPassword3.getText())
-                            + String.valueOf(editTextNumberPassword4.getText());
-                    editTextNumberPassword1.setText("");
-                    editTextNumberPassword2.setText("");
-                    editTextNumberPassword3.setText("");
-                    editTextNumberPassword4.setText("");
-                    pinCodeTextView.setText("Enter your Pin code again");
-                    reEnter = true;
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty()){
+                    editTextNumberPassword2.requestFocus();
+
                 }
-                else{
-                    reDigits = String.valueOf(editTextNumberPassword1.getText())
-                            + String.valueOf(editTextNumberPassword2.getText())
-                            + String.valueOf(editTextNumberPassword3.getText())
-                            + String.valueOf(editTextNumberPassword4.getText());
-                    if (reDigits.equals(digits)){
-                        pinCode.addPinCode(digits);
-                        fingerprintDialog();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        editTextNumberPassword2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty()){
+                    editTextNumberPassword3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        editTextNumberPassword3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty()){
+                    editTextNumberPassword4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        editTextNumberPassword4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty()){
+                    if (pin.isEmpty()){
+                        if(!validationFlag){
+                            digits = readUsersInput();
+                            Log.i("1onTextChanged: ",digits);
+                            emptyTheFields();
+                            pinCodeTextView.setText("Enter your Pin code again");
+                            validationFlag = true;
+                            editTextNumberPassword1.requestFocus();
+                        }
+                        else {
+                            String reDigits = readUsersInput();
+                            Log.i("2onTextChanged: ",reDigits);
+                            if (reDigits.equals(digits)){
+                                pinCode.addPinCode(digits);
+                                fingerprintDialog();
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(),"The Pins don't match",Toast.LENGTH_SHORT).show();
+                                emptyTheFields();
+                                pinCodeTextView.setText("Enter your Pin code");
+                                validationFlag = false;
+                                editTextNumberPassword1.requestFocus();
+                            }
+                        }
 
                     }
                     else{
-                        Toast.makeText(getApplicationContext(),"Pin Codes don't match",Toast.LENGTH_SHORT).show();
-                        editTextNumberPassword1.setText("");
-                        editTextNumberPassword2.setText("");
-                        editTextNumberPassword3.setText("");
-                        editTextNumberPassword4.setText("");
-                        pinCodeTextView.setText("Enter your Pin code");
-                        reEnter= false;
+
+                        digits = readUsersInput();
+                        Log.i("3onTextChanged: ",digits);
+                        if (digits.equals(pin)){
+                            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"The Pin Code is incorrect",Toast.LENGTH_SHORT).show();
+                            emptyTheFields();
+                            editTextNumberPassword1.requestFocus();
+                        }
                     }
+                    emptyTheFields();
                 }
             }
-        });
+            @Override
+            public void afterTextChanged(Editable editable) {
 
+            }
+        });
     }
+
 
     private void biometricAuth() {
 
